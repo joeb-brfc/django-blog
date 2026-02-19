152354619d7e296dcde317237ea01f2e9b833b1b
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-02-19 19:23:40 +0000

    add comment model, migrate database and register in admin

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-# Create your models here.
-
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -16,7 +14,23 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter"
+    )
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.author}"
