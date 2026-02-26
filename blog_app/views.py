5c6f8a06e8b643a67b09d07a067a1b32fc45b0c2
joeb_brfc <joseph_bird93@hotmail.co.uk>
2026-02-26 12:35:27 +0000

    added post_detail view

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,13 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/post_list.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
